b58165e7cd1a41195a5d7964c08ed0f3c8f6d378
HanumanZhang <210585057@qq.com>
2018-11-19 17:48:54 +0800

    pom文件

---
+++ src/test/java/com_mapbar_cross_road_query/com_mapbar_cross_road_query/ComMapbarCrossRoadQueryApplicationTests.java
@@ -0,0 +1,16 @@
+package com_mapbar_cross_road_query.com_mapbar_cross_road_query;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComMapbarCrossRoadQueryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
